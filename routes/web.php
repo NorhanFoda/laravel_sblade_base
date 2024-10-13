cebcb0f7a7d5fa85e98d0862f028962088ca57a3
Khaled Al-Wakeel <k.elwakeel@salasa.co>
2024-10-13 14:13:20 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\Dashboard\NotificationController;
-use App\Http\Controllers\Api\V1\Dashboard\GeneralSettingsController;
 use App\Http\Controllers\V1\Dashboard\HomeController;
 use App\Http\Controllers\V1\Dashboard\RoleController;
 use App\Http\Controllers\V1\Dashboard\UserController;
@@ -13,21 +11,15 @@ use App\Http\Controllers\V1\Dashboard\Auth\LogoutController;
 // -----------------------------START: Auth-----------------------------------
 Route::get('/', [LoginController::class, 'getLoginForm'])->name('login.form');
 Route::post('login', [LoginController::class, 'login'])->name('login');
-// -----------------------------END: Auth-------------------------------------
 
 Route::middleware('auth:web')->group(function() {
     // -----------------------------START: Home-----------------------------------
     Route::get('/home', [HomeController::class, 'index'])->name('home');
-    // -----------------------------END: Home-------------------------------------
-
     // -----------------------------START: Users-----------------------------------
     Route::resource('users', UserController::class);
-    // -----------------------------END: Users-------------------------------------
-
     // -----------------------------START: Roles-----------------------------------
     Route::resource('roles', RoleController::class);
     Route::get('permissions', PermissionController::class);
-    // -----------------------------END: Roles-------------------------------------
 
     Route::post('logout', LogoutController::class)->name('logout');
 });
