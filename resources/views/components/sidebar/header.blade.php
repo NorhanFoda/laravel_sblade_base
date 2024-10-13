57c78bdc42b0754950a73a903ddf77a35ee2cf9d
Khaled Al-Wakeel <k.elwakeel@salasa.co>
2024-10-13 13:50:14 +0300

    add logo

--- resources/views/components/sidebar/header.blade.php
+++ resources/views/components/sidebar/header.blade.php
@@ -1,21 +1,19 @@
 <div class="sidenav-header">
     <i
-      class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-      aria-hidden="true"
-      id="iconSidenav"
+        class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+        aria-hidden="true"
+        id="iconSidenav"
     ></i>
     <a
-      class="navbar-brand m-0"
-      href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
-      target="_blank"
+        class="navbar-brand m-0 text-center"
+        href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
+        target="_blank"
     >
-      <img
-        src="{{asset('UI/assets/v1/img/logo-ct.png')}}"
-        class="navbar-brand-img h-100"
-        alt="main_logo"
-      />
-      <span class="ms-1 font-weight-bold text-white"
-        >Material Dashboard 2</span
-      >
+        <img
+            src="{{asset('UI/assets/v1/img/beetleware-white-logo.png')}}"
+            class="navbar-brand-img w-50"
+            alt="main_logo"
+        />
+{{--        <span class="ms-1 font-weight-bold text-white">Beetleware Dashboard </span>--}}
     </a>
-  </div>
+</div>
